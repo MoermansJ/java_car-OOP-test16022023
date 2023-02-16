72c5e3ee64dc75d58bcf2d35eabf91e39cfce90a
MoermansJ <108662909+MoermansJ@users.noreply.github.com>
2023-02-16 11:20:23 +0100

    bonus++ done

--- src/be/intecbrussel/Car.java
+++ src/be/intecbrussel/Car.java
@@ -3,47 +3,6 @@ package be.intecbrussel;
 import java.util.Random;
 
 public class Car {
-    /*
-    Maak een class aan genaamd Car we willen bij het aanmaken van een instantie de volgende eigenschappen
-    kunnen meegeven kleur, maximum snelheid en paardenkracht. Wanneer we geen specifieke kleur meegeven
-    is de auto standaard wit. De maximale snelheid is standaard 120 en de standaard waarde voor paardenkracht
-    is 150. Zorg ervoor dat alle eigenschappen beschermd zijn. maar dat we deze wel kunnen opvragen.
-    */
-
-    /*
-    Maak een extra variabele aan in de class Car die de huidige snelheid gaat bijhouden. Maak een methode aan
-    speedUp en een methode slowDown wanneer deze methode wordt aangeroepen moet de huidige snelheid
-    optellen met 5 of verminderen met 5. De huidige snelheid mag nooit meer worden dan maximumsnelheid en
-    nooit lager zijn dan 0; Zorg er ook voor dat ik de huidige snelheid kan opvragen. Maak ook nog een methode
-    repaint waarmee ik de kleur van mijn wagen kan aanpassen. Maak een methode aan genaamd repair deze
-    methode moet een random geheel getal genereren tussen 100 en 300, druk vervolgens het random getal af.
-    als volgt: repair cost is 237
-    */
-
-    //Zorg ervoor dat het mogelijk is om alle informatie van de wagen af te drukken. met de tostring() methode.
-
-    //Zorg ervoor dat we de mogelijkheid hebben om het aantal wagens op te vragen dat zijn aangemaakt.
-
-    /*
-    Zorg ervoor dat we de lichten van onze wagen kunnen aan en uit zetten. Dit door middel van één enkel
-    methode toggleLights. Wanneer de lichten uit staan -> zet je deze aan. Als de lichten aan staan -> zet je deze
-    uit. druk een boodschap af met de melding of de lichten aan of uit staan.
-    */
-
-    /*
-    Voeg een extra property toe aan de class Car genaamd driver de standaard waarde is "unknown" Zorg ervoor
-    dat ik een bestuurder aan de wagen kan toekennen met de methode assignDriver(String driverName) de
-    naam van de bestuurder moet minstens 5 karakters lang zijn en mag niet enkel spaties bevatten anders krijg
-    je de melding -> Niet toegestaan.
-    */
-
-    /*
-    Voeg nog een methode toe generateLicensePlate() deze gebruikt het volgende algoritme om een
-    nummerplaat te genereren: De eerste 3 letters van de bestuurder/driver in hoofdletters gevolgd door een
-    streepje (-) met een random getal tussen 100 en 999. Voorbeeld: Driver heeft de naam Quinten dan wordt de
-    nummerplaat: QUI-385 De nummerplaat moet ook opgevraagd kunnen worden met een get methode.
-    */
-
     //instance(s)
     private Random random = new Random();
 
@@ -58,7 +17,7 @@ public class Car {
     private int horsePower = 150;
     private int currentVelocity = 0;
     private boolean lightStatus = false;
-    private String driver = "unknown";
+    private Driver driver = new Driver();
     private String licensePlate = generateLicensePlate();
 
 
@@ -108,6 +67,11 @@ public class Car {
         return licensePlate;
     }
 
+    public String getDriver() {
+        return this.driver.toString();
+    }
+
+
     //custom methods
     public void speedUp() {
         int newVelocity = currentVelocity + 5;
@@ -138,17 +102,11 @@ public class Car {
         System.out.println("The lights are " + (lightStatus ? "on" : "off"));
     }
 
-    public void assignDriver(String driverName) {
-        //naam van de bestuurder moet minstens 5 karakters lang zijn en mag niet enkel spaties bevatten
-        //dwz minstens 4 spaties + 1 ander karakter is OK
-        if (driverName.trim().length() >= 1) {
-            driver = driverName;
-        } else {
-            System.out.println("Niet toegestaan.");
-        }
+    public void assignDriver(Driver driver) {
+        this.driver = driver;
     }
 
-//    public String generateLicensePlate() {
+//    private String generateLicensePlate() {
 //        return driver.substring(0, 3).toUpperCase() + "-" + random.nextInt(100, 1000);
 //    }
 
--- src/be/intecbrussel/Driver.java
+++ src/be/intecbrussel/Driver.java
@@ -1,40 +1,49 @@
-//package be.intecbrussel;
-//
-//public class Driver {
-//    /*
-//    Nu zit de driver als String in de Car class, dit gaan we aanpassen door een driver class aan te maken.
-//    Nu gebruiken we niet meer de String als datatype maar de driver class.
-//    */
-//
-//    //properties
-//    private String name = "unknown";
-//
-//
-//    //constructors
-//    public Driver() {
-//
-//    }
-//
-//    public Driver(String name) {
-//        this.name = name;
-//    }
-//
-//
-//    //getters & setters
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//
-////custom methods
-//    //overrides
-//
-//    @Override
-//    public String toString() {
-//        return "name: " + name;
-//    }
-//}
+package be.intecbrussel;
+
+public class Driver {
+    /*
+    Nu zit de driver als String in de Car class, dit gaan we aanpassen door een driver class aan te maken.
+    Nu gebruiken we niet meer de String als datatype maar de driver class.
+    */
+
+    //properties
+    private String name = "unknown";
+
+
+    //constructors
+    public Driver() {
+
+    }
+
+    public Driver(String name) {
+        setName(name);
+    }
+
+
+    //getters & setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    //custom methods
+    public void assignDriver(String driverName) {
+        //naam van de bestuurder moet minstens 5 karakters lang zijn en mag niet enkel spaties bevatten
+        //dwz minstens 4 spaties + 1 ander karakter is OK
+        if (driverName.trim().length() >= 1) {
+            name = driverName;
+        } else {
+            System.out.println("Niet toegestaan.");
+        }
+    }
+
+    //overrides
+    @Override
+    public String toString() {
+        return "name: " + name;
+    }
+}
