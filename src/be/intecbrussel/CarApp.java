e73781118bcabe0a0d171f2d682bc8c178685a10
MoermansJ <108662909+MoermansJ@users.noreply.github.com>
2023-02-16 13:20:34 +0100

    cleaned up

--- src/be/intecbrussel/CarApp.java
+++ src/be/intecbrussel/CarApp.java
@@ -5,12 +5,6 @@ import com.sun.security.jgss.GSSUtil;
 import java.util.Random;
 
 public class CarApp {
-    /*
-    Maak een CarApp klasse aan die onze main methode zal bevatten. Hier zullen we om te testen 3 Auto's
-    aanmaken. Zet van de eerste Auto de kleur op paars. Laat de 2de auto maximum 70 rijden en heeft de kleur
-    groen. Auto 3 zal getuned zijn met een 200 hp motor een maximum snelheid van 160 de kleur is geel.
-    */
-
     public static void main(String[] args) {
         Car firstCar = new Car();
         Car secondCar = new Car();
